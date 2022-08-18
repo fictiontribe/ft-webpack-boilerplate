cce4b0bfedcdfa94d5d2628f00ffd43c4e9fe494
Connor Burgess <connorburgesscodes@gmail.com>
2022-08-18 15:39:57 -0500

    add gsap

---
+++ src/javascripts/animations.js
@@ -0,0 +1,10 @@
+import gsap from "gsap";
+
+// Gsap Plugins:
+
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger); 
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  //Add animations
+});
